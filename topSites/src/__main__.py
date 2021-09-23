2e07c3eeb18d58e3ee4c191f10f9e98be30c2a35
OliveiraLeonardo17 <oliveira.leonardo.r@gmail.com>
2021-09-23 17:54:48 -0300

    Add __init__ update __main__ topSites

--- topSites/src/__main__.py
+++ topSites/src/__main__.py
@@ -1,76 +1,28 @@
 import psycopg2
-from urllib.parse import urlparse as urlParse
-from collections import Counter
-import json
+from .databaseConfig import config as dbConfig
+from .databaseConnection import open, close
+from .setTopSites import setTopSites
 
-def open_conn():
-  try:
-    conn = psycopg2.connect(
-      host="localhost",
-      port="40000",
-      user="admin",
-      password="12345",
-      dbname="train_of_thought"
-    )
-    print("Conexão aberta")
-    return conn
+def main():
+    conn = None
+    try:
+        params = dbConfig()
 
-  except (Exception, psycopg2.DatabaseError) as error:
-    print(error)
+        print('Connecting to the PostgreSQL database...')
+        conn = open(**params)
+        cur = conn.cursor()
 
-def close_conn(conn):
-  if conn is not None:
-    conn.close()
-    print("Conexão fechada")
+        setTopSites(cur, conn)
 
-def setTopSites():
-  try:
-    conn = open_conn()
-    cur = conn.cursor()
+        conn.commit()
 
-    userIds = getDistinctUserIds(cur)
-    for userId in userIds:
-      capturedUrls = getUserCapturedUrls(cur, userId)
-
-      domains = []
-      for capturedUrl in capturedUrls:
-        domains.append(urlParse(capturedUrl[0]).netloc)
-
-      topSites = json.dumps(dict(Counter(domains).most_common(10)))
-
-      hasTopSites = countUserTopSites(cur, userId)
-
-      if hasTopSites[0] == 0:
-        insertTopSites(cur, userId, topSites)
-      else:
-        updateTopSites(cur, userId, topSites)
-
-      conn.commit()
-      print(f"Dados commitados para userId {userId[0]}")
-
-  except (Exception, psycopg2.DatabaseError) as error:
-    print(error)
-  finally:
-    close_conn(conn)
-
-def getDistinctUserIds(cur):
-  cur.execute('SELECT DISTINCT "userId" FROM "Captures"')
-  return cur.fetchall()
-
-def getUserCapturedUrls(cur, userId):
-  cur.execute(f'SELECT "capturedUrl" FROM "Captures" WHERE "userId" = {userId[0]}')
-  return cur.fetchall()
-
-def countUserTopSites(cur, userId):
-  cur.execute(f'SELECT COUNT("userId") FROM "TopSites" WHERE "userId" = {userId[0]}')
-  return cur.fetchone()
-
-def insertTopSites(cur, userId, topSites):
-  cur.execute(f'INSERT INTO "TopSites" VALUES({userId[0]}, \'{topSites}\')')
-
-def updateTopSites(cur, userId, topSites):
-  cur.execute(f'UPDATE "TopSites" SET "topSites" = \'{topSites}\' WHERE "userId" = {userId[0]}')
+        cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            close(conn)
 
 
 if __name__ == '__main__':
-  setTopSites()
+  main()
